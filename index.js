d5895904ec398a8c7f74e22df61a7a96ed67cff9
Guilherme Okushi <guiokushi@hotmail.com>
2022-02-19 12:28:28 -0300

    retomar da seção 1.2. Falando de tipos

---
+++ index.js
@@ -0,0 +1,14 @@
+var linguagem = "Javascript"
+console.log("Aprendendo " + linguagem)
+//variavel pode ser redeclarada
+var linguagem = "Java "
+console.log("Aprendendo" + linguagem);
+
+var idade = 23
+console.log("Oi, " + nome);
+if (idade >= 18) {
+    var nome = "João"
+    console.log("Parabéns, " + nome);
+    console.log("Você pode dirigir!");
+}
+console.log("Até mais, " + nome);
